ca06155ee5c18fe4798ebd00040b42b845cc3f61
Matt Mikula <matthewmikula@gmail.com>
2018-06-11 07:39:46 -0400

    Add documentation to EmployeeCreatorManager class

--- src/main/java/com/matt/employees/strategies/EmployeeCreatorManager.java
+++ src/main/java/com/matt/employees/strategies/EmployeeCreatorManager.java
@@ -8,11 +8,12 @@ import java.util.*;
 /**
  * Singleton class that handles keeping track of registered {@link EmployeeCreator} strategies.
  * We use a singleton here so that EmployeeCreator strategies only have to be registered once and can be accessed anywhere in
- * our code instead of having to instantiate a manager and register strategies whenever we'd like to use load employees.
+ * our code. If we didn't use a singleton, we would have to instantiate a manager and register strategies whenever
+ * we'd like to use load {@link Employee} objects using this class.
  *
- * Registered EmployeeCreators are stored in an internal hash with the file extension as a key.
+ * Registered EmployeeCreators are stored in an internal map with the file extension as a key.
  * A path to the file is supplied when creating employees via the createEmployees method, and the file's extension is
- * checked against the internal hash to find a creator object. If a creator is found, it is called to generate a List of
+ * checked against the internal map to find a creator object. If a creator is found, it is called to generate a List of
  * employees. If a creator is not found, an IllegalArgumentException is thrown.
  */
 public final class EmployeeCreatorManager {
@@ -24,14 +25,35 @@ public final class EmployeeCreatorManager {
     // Private constructor to prevent instantiation
     private EmployeeCreatorManager() {}
 
+    /**
+     * Static method used to access our private singleton instance.
+     *
+     * @return - {@link EmployeeCreator} singleton instance
+     */
     public static EmployeeCreatorManager getInstance() {
         return instance;
     }
 
+    /**
+     * Handles storing a file extension and an {@link EmployeeCreator} in our internal map
+     *
+     * @param extension - File extension to be associated with the supplied {@link EmployeeCreator}
+     * @param creator - An {@link EmployeeCreator} instance that may be used to create {@link Employee} objects
+     */
     public void registerCreator(String extension, EmployeeCreator creator) {
         creators.put(extension.toLowerCase(), creator);
     }
 
+    /**
+     * Accepts a file path and checks the file's extension against our internal map. If an {@link EmployeeCreator} is
+     * associated with the supplied file's extension, we'll use that creator to create a list of {@link Employee} objects.
+     * If no creator is associated with the extension, we'll throw an IllegalArgumentException.
+     *
+     * @param path - Path to a file that will be used to create {@link Employee} objects
+     * @return - A list of newly created {@link Employee} objects
+     * @throws IllegalArgumentException - Will be thrown if we can't find a registered {@link EmployeeCreator} strategy
+     * for the given file path
+     */
     public List<Employee> createEmployees(String path) throws IllegalArgumentException {
         String extension = FilenameUtils.getExtension(path).toLowerCase();
 
